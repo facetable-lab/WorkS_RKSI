68dbeee0566933ecec6ce675b898be6a3441f22e
facetable <F201283070480f>
2022-10-04 00:24:12 +0300

    Русификация приложения для админ-панели

--- src/search_engine/apps.py
+++ src/search_engine/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SearchEngineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'search_engine'
+    verbose_name = 'Поисковое приложение (search engine)'
