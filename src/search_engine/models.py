b3972f390adb7bbcf59fad55b594a7d6dc93a3c7
facetable <F201283070480f>
2022-10-04 00:20:04 +0300

    Русификация для админки

--- src/search_engine/models.py
+++ src/search_engine/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class City(models.Model):
-    designation = models.CharField(max_length=50)
+    designation = models.CharField(max_length=50, verbose_name='Название города')
     slug = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.designation
