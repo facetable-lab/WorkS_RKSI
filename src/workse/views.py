e8b389e5dad4b785e143e6f23242c141960a6ddc
facetable <F201283070480f>
2022-10-03 23:16:54 +0300

    Первый коммит. Создал проект Django.

---
+++ src/workse/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+import datetime
+
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'Dima'
+
+    context = {
+        'date': date,
+        'name': name
+    }
+
+    return render(request, 'home.html', context)
