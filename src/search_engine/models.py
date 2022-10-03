b0b1b237cbf472b03d6630524dceb70c71fcf7ab
facetable <F201283070480f>
2022-10-04 00:39:00 +0300

    Добавил модель вакансии

--- src/search_engine/models.py
+++ src/search_engine/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 
 class City(models.Model):
-    designation = models.CharField(max_length=50, verbose_name='Название города')
-    slug = models.CharField(max_length=50, blank=True)
+    designation = models.CharField(max_length=50, unique=True, verbose_name='Название города')
+    slug = models.CharField(max_length=50, unique=True, blank=True)
 
     class Meta:
         verbose_name = 'город'
         verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.designation
+
+
+class Vacancy(models.Model):
+    designation = models.CharField(max_length=50, unique=True, verbose_name='Вакансия')
+    slug = models.CharField(max_length=50, unique=True, blank=True)
+
+    class Meta:
+        verbose_name = 'вакансию'
+        verbose_name_plural = 'Вакансии'
 
     def __str__(self):
         return self.designation
