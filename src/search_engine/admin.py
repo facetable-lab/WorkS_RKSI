20a44c33c69121ab5b6efb416ac76f6fcbd9b9d1
facetable <F201283070480f>
2022-10-04 16:46:13 +0300

    Зарегистрировал модел собранной вакансии в админ-панель

--- src/search_engine/admin.py
+++ src/search_engine/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import City, Vacancy
+from .models import City, Vacancy, VacancyParsed
 
 admin.site.register(City)
 admin.site.register(Vacancy)
+admin.site.register(VacancyParsed)
