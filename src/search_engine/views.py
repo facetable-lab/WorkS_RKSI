5e39c828c8e2e5e7cbf62e66af9ebef4e716d020
facetable <F201283070480f>
2022-10-09 23:27:55 +0300

    Переименовал список объектов

--- src/search_engine/views.py
+++ src/search_engine/views.py
@@ -6,6 +6,6 @@ from .models import VacancyParsed
 def home_view(request):
     vacancy_objects = VacancyParsed.objects.all()
     context = {
-        'object_list': vacancy_objects
+        'vacancies_list': vacancy_objects
     }
     return render(request, 'home.html', context=context)
