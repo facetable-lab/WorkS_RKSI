2d9b5b93c4d896644f497d858e851644637823b3
facetable <F201283070480f>
2022-10-10 00:25:17 +0300

    Вынес код в базовый шаблон

--- src/workse/views.py
+++ src/workse/views.py
@@ -1,15 +1,1 @@
 from django.shortcuts import render
-
-import datetime
-
-
-def home(request):
-    date = datetime.datetime.now().date()
-    name = 'Dima'
-
-    context = {
-        'date': date,
-        'name': name
-    }
-
-    return render(request, 'home.html', context)
