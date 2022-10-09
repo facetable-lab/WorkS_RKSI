9e9c7c2a60e835d0398f53306ae02d995f6f400d
facetable <F201283070480f>
2022-10-10 02:32:39 +0300

    Логика отображения вакансий согласно фильтрам в форме

--- src/search_engine/views.py
+++ src/search_engine/views.py
@@ -4,7 +4,16 @@ from .models import VacancyParsed
 
 
 def home_view(request):
-    vacancy_objects = VacancyParsed.objects.all()
+    searched_city = request.GET.get('city')
+    searched_specialization = request.GET.get('specialization')
+    filtered_vacancies = {}
+    if searched_city or searched_specialization:
+        if searched_city:
+            filtered_vacancies['city__designation'] = searched_city
+        if searched_specialization:
+            filtered_vacancies['vacancy__designation'] = searched_specialization
+
+    vacancy_objects = VacancyParsed.objects.filter(**filtered_vacancies)
     context = {
         'vacancies_list': vacancy_objects
     }
