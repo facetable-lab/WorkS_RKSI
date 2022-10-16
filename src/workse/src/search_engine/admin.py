a785b7be046dabd9bab607ce6b2e78076934f0f3
facetable <F201283070480f>
2022-10-17 00:16:11 +0300

    Регистрация моделей в админке, генерация slug'а

--- src/workse/src/search_engine/admin.py
+++ src/workse/src/search_engine/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import City, Specialization, Vacancy
+
+
+class CityAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class SpecializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Specialization, SpecializationAdmin)
+admin.site.register(Vacancy)
