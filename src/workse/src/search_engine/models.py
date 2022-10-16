7e18f6e20fa410a714e8d1d8208d7bc64382e94d
facetable <F201283070480f>
2022-10-17 00:17:03 +0300

    Модель города, модель специализации, модель вакансии

--- src/workse/src/search_engine/models.py
+++ src/workse/src/search_engine/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    title = models.CharField(max_length=50, unique=True, verbose_name='Город')
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'город'
+        verbose_name_plural = 'города'
+
+
+class Specialization(models.Model):
+    title = models.CharField(max_length=50, unique=True, verbose_name='Специализация')
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'специализацию'
+        verbose_name_plural = 'Специализации'
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True, verbose_name='Ссылка (url)')
+    title = models.CharField(max_length=70, verbose_name='Название вакансии')
+    company = models.CharField(max_length=70, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey(City, on_delete=models.PROTECT, verbose_name='Город')
+    specialization = models.ForeignKey(Specialization, on_delete=models.PROTECT, verbose_name='Специализация')
+    time_stamp = models.DateField(auto_now_add=True, verbose_name='Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'вакансию'
+        verbose_name_plural = 'Вакансии'
