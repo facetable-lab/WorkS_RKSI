bbade317780b10d62ae1c04091d31f963cae20f4
facetable <F201283070480f>
2022-10-17 01:19:17 +0300

    Функция отображения главной страницы с передачей списка

--- src/workse/src/search_engine/views.py
+++ src/workse/src/search_engine/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Vacancy
+
+
+def index(request):
+    vacancies_list = Vacancy.objects.all()
+    context = {
+        'vacancies_list': vacancies_list
+    }
+    return render(request, 'index.html', context)
