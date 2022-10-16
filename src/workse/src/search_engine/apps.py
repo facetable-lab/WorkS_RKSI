7b9bd042f6bfdb29a8140b426e95a218aa1d7228
facetable <F201283070480f>
2022-10-17 00:16:30 +0300

    Название приложения, для админки

--- src/workse/src/search_engine/apps.py
+++ src/workse/src/search_engine/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class SearchEngineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'search_engine'
+
+    verbose_name = 'Сбор вакансий (search_engine)'
