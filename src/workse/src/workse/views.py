de9ab578425ae622d701dc57e92344f85d3c3f7a
facetable <F201283070480f>
2022-10-17 01:20:17 +0300

    Убрал старую функцию отображения

--- src/workse/src/workse/views.py
+++ src/workse/src/workse/views.py
@@ -1,11 +1,1 @@
-from django.shortcuts import render
-
-import datetime
-
-
-def index(request):
-    date = datetime.datetime.now()
-    context = {
-        'date': date
-    }
-    return render(request, 'index.html', context)
+# from django.shortcuts import render
