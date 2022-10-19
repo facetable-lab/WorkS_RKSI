6f19a74d0948a63db9265fba7d6792a9cc16f423
facetable <F201283070480f>
2022-10-20 00:28:22 +0300

    Передача формы в шаблон для ее вывода

--- src/workse/src/search_engine/views.py
+++ src/workse/src/search_engine/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 
 from .models import Vacancy
+from .forms import SearchForm
 
 
 def index(request):
+    form = SearchForm()
     city = request.GET.get('city')
     specialization = request.GET.get('specialization')
     _filter = {}
     if city or specialization:
         if city:
-            _filter['city__title'] = city
+            _filter['city__slug'] = city
         if specialization:
-            _filter['specialization__title'] = specialization
+            _filter['specialization__slug'] = specialization
 
     vacancies_list = Vacancy.objects.filter(**_filter)
 
     context = {
-        'vacancies_list': vacancies_list
+        'vacancies_list': vacancies_list,
+        'form': form
     }
     return render(request, 'search_engine/index.html', context)
