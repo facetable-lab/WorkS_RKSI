101c54ed99609b030bcad82fe076f4c84dd1bde3
facetable <F201283070480f>
2022-10-24 17:14:38 +0300

    Сохранение ошибок в БД, todo рефакторинг

--- src/workse/src/run_parser.py
+++ src/workse/src/run_parser.py
@@ -3,6 +3,7 @@ import codecs
 import os
 import sys
 
+# Инициализация django, для работы с моделями.
 project_src = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(project_src)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'workse.settings'
@@ -11,7 +12,7 @@ import django
 
 django.setup()
 
-from search_engine.models import City, Specialization, Vacancy
+from search_engine.models import City, Specialization, Vacancy, Error
 from search_engine.parser import *
 
 parsers = (
@@ -24,22 +25,28 @@ specialization = Specialization.objects.filter(slug='python').first()
 
 jobs, errors = [], []
 
+# Запуск сбора.
 for func, url in parsers:
     j, e = func(url)
     jobs += j
     errors += e
 
 # Отчистка памяти от лишних объектов.
+# TODO: refactor with if errors and delete
 del j, e, parsers
-if len(errors) == 0:
-    del errors
+# if len(errors) == 0:
+#     del errors
 
+# Запись собранных вакансий в БД.
 for job in jobs:
     vacancy = Vacancy(**job, city=city, specialization=specialization)
     try:
         vacancy.save()
     except DatabaseError:
         pass
+
+if errors:
+    er = Error(data=errors).save()
 
 # file_handler = codecs.open('vacancies.txt', 'w', 'utf-8')
 # file_handler.write(str(jobs))
