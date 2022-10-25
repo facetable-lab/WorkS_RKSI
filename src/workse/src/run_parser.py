e839b5c5b00ac6e390de30d646b52d607ead56ff
facetable <F201283070480f>
2022-10-26 00:40:52 +0300

    Получение настроек пользователя из БД. Автоматизация

--- src/workse/src/run_parser.py
+++ src/workse/src/run_parser.py
@@ -1,4 +1,5 @@
 from django.db import DatabaseError
+from django.contrib.auth import get_user_model
 import codecs
 import os
 import sys
@@ -12,13 +13,38 @@ import django
 
 django.setup()
 
-from search_engine.models import City, Specialization, Vacancy, Error
+from search_engine.models import City, Specialization, Vacancy, Error, Url
 from search_engine.parser import *
+
+Subscriber = get_user_model()
 
 parsers = (
     (head_hunter, 'https://persianovskiy.hh.ru/search/vacancy?text=python&from=suggest_post&area=1'),
-    (habr_career, 'https://career.habr.com/vacancies?locations%5B%5D=c_678&q=java&type=all')
+    (habr_career, 'https://career.habr.com/vacancies?locations%5B%5D=c_678&q=python&type=all')
 )
+
+
+def get_subscriber_settings():
+    settings_filtered = Subscriber.objects.filter(is_mailing=True).values()
+    settings_list = set((sf['city_id'], sf['specialization_id']) for sf in settings_filtered)
+    return settings_list
+
+
+def get_urls(_settings):
+    urls = []
+    urls_qs = Url.objects.all().values()
+    url_dict = {(uqs['city_id'], uqs['specialization_id']): uqs['url_data'] for uqs in urls_qs}
+
+    for pair in _settings:
+        # TODO: Переписать код компактнее
+        tmp = {}
+        tmp['city'] = pair[0]
+        tmp['specialization'] = pair[1]
+        tmp['url_data'] = url_dict[pair]
+        urls.append(tmp)
+
+    return urls
+
 
 city = City.objects.filter(slug='moskva').first()
 specialization = Specialization.objects.filter(slug='python').first()
@@ -32,7 +58,7 @@ for func, url in parsers:
     errors += e
 
 # Отчистка памяти от лишних объектов.
-# TODO: refactor with if errors and delete
+# TODO: refactor with if errors and delete on line 48
 del j, e, parsers
 # if len(errors) == 0:
 #     del errors
@@ -40,10 +66,10 @@ del j, e, parsers
 # Запись собранных вакансий в БД.
 for job in jobs:
     vacancy = Vacancy(**job, city=city, specialization=specialization)
-    try:
-        vacancy.save()
-    except DatabaseError:
-        pass
+try:
+    vacancy.save()
+except DatabaseError:
+    pass
 
 if errors:
     er = Error(data=errors).save()
