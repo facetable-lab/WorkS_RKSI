650b3a6c401192fc5b876ce48d2f150894b53d08
facetable <F201283070480f>
2022-10-26 03:39:52 +0300

    Добавил маршрут к списку вакансий

--- src/workse/src/workse/urls.py
+++ src/workse/src/workse/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from search_engine.views import index
+from search_engine.views import index, list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('list', list_view, name='list'),
     path('', index, name='index'),
 ]
