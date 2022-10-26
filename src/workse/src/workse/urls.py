157178994c94c55d538f8200c832489f01520abf
facetable <F201283070480f>
2022-10-27 02:53:19 +0300

    Подключил маршруты приложения accounts

--- src/workse/src/workse/urls.py
+++ src/workse/src/workse/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from search_engine.views import index, list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('list', list_view, name='list'),
+    path('list/', list_view, name='list'),
+    path('accounts/', include(('accounts.urls', 'accounts'))),
     path('', index, name='index'),
 ]
